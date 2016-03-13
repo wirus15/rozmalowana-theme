9456fe05e549ee3de1aff96883053984bcfaab27
Maciej Krawczyk <wirus15@gmail.com>
2016-03-13 14:05:24 +0100

    wywalony slider, poprawki w css

--- header.php
+++ header.php
@@ -22,7 +22,8 @@
     <div class="header-inner">
         <div class="logo">
             <a href="<?php echo esc_url(home_url('/')); ?>">
-                <img src="<?php echo get_template_directory_uri(); ?>/images/rozmalowana_logo.png" alt="Rozmalowana - LOGO"/>
+                <img src="<?php echo get_template_directory_uri(); ?>/images/rozmalowana_logo.png"
+                     alt="Rozmalowana - LOGO"/>
             </a>
         </div><!-- logo -->
         <div class="toggle">
@@ -34,156 +35,10 @@
     </div><!-- header-inner -->
 </div><!-- header -->
 
-<?php if (is_front_page() && is_home()) { ?>
-    <section id="home_slider">
-        <?php
-        $sldimages = '';
-        $sldimages = array(
-            '1' => get_template_directory_uri() . '/images/slides/slider1.jpg',
-            '2' => get_template_directory_uri() . '/images/slides/slider2.jpg',
-            '3' => get_template_directory_uri() . '/images/slides/slider3.jpg',
-        ); ?>
-
-        <?php
-        $slAr = array();
-        $m = 0;
-        for ($i = 1; $i < 4; $i++) {
-            if (get_theme_mod('slide_image' . $i, $sldimages[$i]) != "") {
-                $imgSrc = get_theme_mod('slide_image' . $i, $sldimages[$i]);
-                $imgTitle = get_theme_mod('slide_title' . $i);
-                $imgDesc = get_theme_mod('slide_desc' . $i);
-                $imgLink = get_theme_mod('slide_link' . $i);
-                if (strlen($imgSrc) > 3) {
-                    $slAr[$m]['image_src'] = get_theme_mod('slide_image' . $i, $sldimages[$i]);
-                    $slAr[$m]['image_title'] = get_theme_mod('slide_title' . $i);
-                    $slAr[$m]['image_desc'] = get_theme_mod('slide_desc' . $i);
-                    $slAr[$m]['image_link'] = get_theme_mod('slide_link' . $i);
-                    $m++;
-                }
-            }
-        }
-        $slideno = array();
-        if ($slAr > 0) {
-            $n = 0; ?>
-            <div class="slider-wrapper theme-default">
-                <div id="slider" class="nivoSlider">
-                    <?php
-                    foreach ($slAr as $sv) {
-                        $n++; ?><img src="<?php echo esc_url($sv['image_src']); ?>"
-                                     alt="<?php echo esc_attr($sv['image_title']); ?>"
-                                     title="<?php echo esc_attr('#slidecaption' . $n); ?>" /><?php
-                        $slideno[] = $n;
-                    }
-                    ?>
-                </div><?php
-
-
-                foreach ($slideno as $sln) { ?>
-                <div id="slidecaption<?php echo $sln; ?>" class="nivo-html-caption">
-                    <div class="slide_info">
-                        <h2>
-                            <a href="<?php echo esc_url(get_theme_mod('slide_link' . $sln, '#link' . $sln)); ?>"><?php echo get_theme_mod('slide_title' . $sln, 'Living and Lifestyle Title'); ?></a>
-                        </h2>
-                        <?php if (get_theme_mod('slide_desc' . $sln, '')) { ?>
-                            <p><?php echo get_theme_mod('slide_desc' . $sln, ''); ?></p>
-                        <?php } ?>
-
-                        <?php if (get_theme_mod('slide_link' . $sln, '')) { ?>
-                            <a class="morelink"
-                               href="<?php echo esc_url(get_theme_mod('slide_link' . $sln, '' . $sln)); ?>"><?php _e('Read More', 'skt-girlie'); ?></a>
-                        <?php } ?>
-                    </div>
-                    </div><?php
-                } ?>
-            </div>
-            <div class="clear"></div><?php
-        }
-        ?>
-    </section>
-    <?php
-} elseif (is_front_page()) {
-// Condition For Static Front Page
-    ?>
-    <section id="home_slider">
-        <?php
-        $sldimages = '';
-        $sldimages = array(
-            '1' => get_template_directory_uri() . '/images/slides/slider1.jpg',
-            '2' => get_template_directory_uri() . '/images/slides/slider2.jpg',
-            '3' => get_template_directory_uri() . '/images/slides/slider3.jpg',
-        ); ?>
-
-        <?php
-        $slAr = array();
-        $m = 0;
-        for ($i = 1; $i < 4; $i++) {
-            if (get_theme_mod('slide_image' . $i, $sldimages[$i]) != "") {
-                $imgSrc = get_theme_mod('slide_image' . $i, $sldimages[$i]);
-                $imgTitle = get_theme_mod('slide_title' . $i);
-                $imgDesc = get_theme_mod('slide_desc' . $i);
-                $imgLink = get_theme_mod('slide_link' . $i);
-                if (strlen($imgSrc) > 3) {
-                    $slAr[$m]['image_src'] = get_theme_mod('slide_image' . $i, $sldimages[$i]);
-                    $slAr[$m]['image_title'] = get_theme_mod('slide_title' . $i);
-                    $slAr[$m]['image_desc'] = get_theme_mod('slide_desc' . $i);
-                    $slAr[$m]['image_link'] = get_theme_mod('slide_link' . $i);
-                    $m++;
-                }
-            }
-        }
-        $slideno = array();
-        if ($slAr > 0) {
-            $n = 0; ?>
-            <div class="slider-wrapper theme-default">
-                <div id="slider" class="nivoSlider">
-                    <?php
-                    foreach ($slAr as $sv) {
-                        $n++; ?><img src="<?php echo esc_url($sv['image_src']); ?>"
-                                     alt="<?php echo esc_attr($sv['image_title']); ?>"
-                                     title="<?php echo esc_attr('#slidecaption' . $n); ?>" /><?php
-                        $slideno[] = $n;
-                    }
-                    ?>
-                </div><?php
-
-
-                foreach ($slideno as $sln) { ?>
-                <div id="slidecaption<?php echo $sln; ?>" class="nivo-html-caption">
-                    <div class="slide_info">
-                        <h2>
-                            <a href="<?php echo esc_url(get_theme_mod('slide_link' . $sln, '#link' . $sln)); ?>"><?php echo get_theme_mod('slide_title' . $sln, 'Splendid Days'); ?></a>
-                        </h2>
-                        <?php if (get_theme_mod('slide_desc' . $sln, '')) { ?>
-                            <p><?php echo get_theme_mod('slide_desc' . $sln, ''); ?></p>
-                        <?php } ?>
-
-                        <?php if (get_theme_mod('slide_link' . $sln, '')) { ?>
-                            <a class="ReadMore"
-                               href="<?php echo esc_url(get_theme_mod('slide_link' . $sln, '' . $sln)); ?>"><?php _e('Read More', 'skt-girlie'); ?></a>
-                        <?php } ?>
-                    </div>
-                    </div><?php
-                } ?>
-            </div>
-            <div class="clear"></div><?php
-        }
-        ?>
-    </section>
-    <?php
-// Condition For Static Front Page >> Blog Page
-} elseif (is_home()) { ?>
-    <div class="space30"></div>
-    <section id="home_slider" style="display:none;"></section>
-    <?php
-// Condition For Other Page
-} else {
-    $header_image = get_header_image();
-    if (!empty($header_image)) {
-        echo ' <div class="innerbanner"><img src="' . esc_url($header_image) . '" width="' . get_custom_header()->width . '" height="' . get_custom_header()->height . '" alt="" /> </div>';
-    } else {
-        echo '<div class="space30"></div>';
+<?php
+    if (!empty($header_image = get_header_image())) {
+        echo '<div class="innerbanner"><img src="' . esc_url($header_image) . '" width="' . get_custom_header()->width . '" height="' . get_custom_header()->height . '" alt="" /></div>';
     }
-}
 ?>
 
 <?php if (is_front_page() && !is_home()) { ?>
@@ -200,6 +55,7 @@
                         } ?>">
                             <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?>
                                 <h4><?php the_title(); ?></h4></a>
+
                             <p><?php the_excerpt(); ?></p>
                             <a class="morelink"
                                href="<?php the_permalink(); ?>"><?php _e('Read More', 'skt-girlie'); ?></a>
@@ -214,6 +70,7 @@
                                 src="<?php echo get_template_directory_uri(); ?>/images/about<?php echo $p; ?>.jpg"
                                 alt=""/>
                             <h4><?php _e('Page Title', 'skt-girlie'); ?><?php echo $p; ?></h4></a>
+
                         <p><?php _e('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque augue eros, posuere a condimentum sit amet, rhoncus eu libero. Maecenas in tincidunt turpis, ut rhoncus neque.', 'skt-girlie'); ?></p>
                         <a class="morelink" href="#"><?php _e('Read More', 'skt-girlie'); ?></a>
                     </div>
--- sidebar.php
+++ sidebar.php
@@ -5,29 +5,21 @@
  * @package SKT Girlie
  */
 ?>
-<div id="sidebar">    
-    <?php if ( ! dynamic_sidebar( 'sidebar-1' ) ) : ?>
-    
-        <h3 class="widget-title"><?php _e( 'Category', 'skt-girlie' ); ?></h3>
-        <aside id="categories" class="widget">           
+<div id="sidebar">
+    <?php if (!dynamic_sidebar('sidebar-1')) : ?>
+
+        <h3 class="widget-title"><?php _e('Category', 'skt-girlie'); ?></h3>
+        <aside id="categories" class="widget">
             <ul>
-                <?php wp_list_categories('title_li=');  ?>
+                <?php wp_list_categories('title_li='); ?>
             </ul>
         </aside>
-        
-       <h3 class="widget-title"><?php _e( 'Archives', 'skt-girlie' ); ?></h3>
-        <aside id="archives" class="widget">           
+
+        <h3 class="widget-title"><?php _e('Archives', 'skt-girlie'); ?></h3>
+        <aside id="archives" class="widget">
             <ul>
-                <?php wp_get_archives( array( 'type' => 'monthly' ) ); ?>
+                <?php wp_get_archives(array('type' => 'monthly')); ?>
             </ul>
         </aside>
-         <h3 class="widget-title"><?php _e( 'Meta', 'skt-girlie' ); ?></h3>
-         <aside id="meta" class="widget">           
-            <ul>
-                <?php wp_register(); ?>
-                <li><?php wp_loginout(); ?></li>
-                <?php wp_meta(); ?>
-            </ul>
-        </aside>
-    <?php endif; // end sidebar widget area ?>	
+    <?php endif; // end sidebar widget area ?>
 </div><!-- sidebar -->
